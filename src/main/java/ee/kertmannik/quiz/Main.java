94b197080bb031e0449e4655efd41443358845ce
Kert Männik <kert.mannik@fortumo.com>
2017-10-12 10:59:16 +0300

    Trying to fix running out of memory in Heroku.

--- src/main/java/ee/kertmannik/quiz/Main.java
+++ src/main/java/ee/kertmannik/quiz/Main.java
@@ -5,7 +5,8 @@ import org.eclipse.jetty.server.Server;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        Server server = new Server(8080);
+        int port = Integer.parseInt(System.getenv("PORT"));
+        Server server = new Server(port);
         server.setHandler(new HelloWorldHandler());
         server.start();
         server.join();
